fd191ba649722f2f767c8e1a2a73abc10f64b42b
Ramzi BENALAYA <benalaya.ramzi@gmail.com>
2018-05-19 13:52:17 +0200

    empty project

---
+++ src/main/java/com/ktg/git/App.java
@@ -0,0 +1,13 @@
+package com.ktg.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
