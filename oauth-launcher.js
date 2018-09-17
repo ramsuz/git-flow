7dee535f40ec8d7fad81847e60a0752f3e0e9931
ramsuz <benalaya.ramzi@gmail.com>
2018-09-17 08:20:51 +0200

    Create oauth-launcher.js
    oauth-launcher.js

---
+++ oauth-launcher.js
@@ -0,0 +1,90 @@
+import { inject } from 'aurelia-framework';
+import { HttpClient, json } from 'aurelia-fetch-client';
+import ErrorUtils from '../utils/errorUtils';
+
+import {LogManager} from 'aurelia-framework';
+let logger = LogManager.getLogger('OAuthLauncher');
+
+@inject(HttpClient)
+export default class OAuthLauncher {
+  constructor(httpClient) {
+    const baseUrl = "http://localhost:8333/api/";
+
+    httpClient.configure(config => {
+      config
+        .withBaseUrl(baseUrl);
+        //.withInterceptor(this.authService.tokenInterceptor);
+    });
+    this.httpClient = httpClient;
+  }
+
+  /**
+   * Model data 
+   * {'success': true/false, 'errorCode': ''}
+   */
+  async init() {
+    logger.debug('init oauth-launcher');
+    var data = await this.callWsRest();
+    if (!data.success && data.status === 403) {
+      data = await this.getOAuthCode();
+      if (data.code) {
+        data = await this.getOAuthToken();
+        if(data.success)
+          return {'isAuthenticated': true};
+      } 
+    }
+
+    ErrorUtils.handleOAuthErrors(data);
+
+    return {'isAuthenticated': false};
+  }
+
+  callWsRest() {
+    return this.httpClient.fetch('users')
+    .then(response => {
+      if(response.status === 403)
+        return {success: false, status: 403, errorCode: 'NO_TOKEN_PROVIDED'};
+      else 
+        return response.json();
+    })
+    .catch(error => {
+      return {success: false, errorCode: 'RESOURCE_SERVER_UNAVAILABLE', errorStack: error};
+    });
+  }
+
+  getOAuthCode() {
+    let oAuthCode = new Promise(function(resolve, reject) {
+      setTimeout(function() { 
+        resolve({'code':'XSDTFFZ'});
+      }, 50);
+    });
+    return oAuthCode; 
+  }
+
+  getOAuthToken() {
+    let userName = 'Gandalf'; 
+    let password = 'password3';
+
+    return this.httpClient.fetch('token', {
+      method: 'post',
+      body: json({ name: userName, password: password })
+    })
+      .then(response => response.json())
+      .then(tokenResult => {
+        if (tokenResult.success) window.localStorage.setItem("token", tokenResult.token);
+        return tokenResult;
+      })
+      .catch(error => {
+        return {success: false, errorCode: 'OAUTH_SERVER_UNAVAILABLE', errorStack: error};
+      });
+  }
+
+  promiseInit() {
+    let root = new Promise(function(resolve, reject) {
+      setTimeout(function() { 
+        resolve('app');
+      }, 50);
+    });
+    return root;
+  }
+}
